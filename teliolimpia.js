591422be463c99325d9c6516b5271e461a64e9d7
HorvaiDaniel <hordan163@hengersor.hu>
2025-02-27 09:15:30 +0100

    Get hiba javítása
    app.get("/", (req, res) =>{
        res.send("Működik a szerver.")
    })

--- teliolimpia.js
+++ teliolimpia.js
@@ -6,9 +6,12 @@ const bodyparser = require("body-parser");
 
 app.use(bodyparser.json())
 
+app.get("/", (req, res) =>{
+    res.send("Működik a szerver.")
+})
 
 
 app.listen(3000, () => {
-    
+
     console.log("A téliolimpia szervere a 3000-es porton fut")
 })
